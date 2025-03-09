d186587fd6e7bb52d7f5e7fa7aa966cfa5e66b77
Akkicodes <akkida746@gmail.com>
2025-03-09 21:40:45 +0000

    reducing zip file size to deploy on lambda

---
+++ function/index.mjs
@@ -0,0 +1,55 @@
+import packageJson from '@aws-sdk/client-s3/package.json' with { type: 'json' };
+import {
+  paginateListBuckets,
+  S3Client,
+  S3ServiceException,
+} from "@aws-sdk/client-s3";
+
+export const handler = async (event) => {
+  
+  console.log(event);
+  console.log("Working changes for lambda function");
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Hello from Lambda!!!'),
+  };
+
+  const client = new S3Client({});
+  /** @type {?import('@aws-sdk/client-s3').Owner} */
+  let Owner = null;
+
+  /** @type {import('@aws-sdk/client-s3').Bucket[]} */
+  const Buckets = [];
+
+  try {
+    const paginator = paginateListBuckets({ client }, {});
+
+    for await (const page of paginator) {
+      if (!Owner) {
+        Owner = page.Owner;
+      }
+
+      Buckets.push(...page.Buckets);
+    }
+
+    console.log(
+      `${Owner.DisplayName} owns ${Buckets.length} bucket${
+        Buckets.length === 1 ? "" : "s"
+      }:`,
+    );
+    console.log(`${Buckets.map((b) => ` • ${b.Name}`).join("\n")}`);
+  } catch (caught) {
+    if (caught instanceof S3ServiceException) {
+      console.error(
+        `Error from S3 while listing buckets.  ${caught.name}: ${caught.message}`,
+      );
+    } else {
+      throw caught;
+    }
+  }
+
+  return response;
+};
+
+
