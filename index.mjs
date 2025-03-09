5bb11dd7478d13d474149d1e0a478675aa1cf11c
Akkicodes <akkida746@gmail.com>
2025-03-09 20:25:19 +0000

    add logs

--- index.mjs
+++ index.mjs
@@ -1,7 +1,9 @@
 import packageJson from '@aws-sdk/client-s3/package.json' with { type: 'json' };
 
 export const handler = async (event) => {
-  // TODO implement
+  
+  console.log("Inside lambda handler");
+
   const response = {
     statusCode: 200,
     body: JSON.stringify('Hello from Lambda!'),
